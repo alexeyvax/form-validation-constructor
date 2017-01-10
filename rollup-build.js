d5fa7916a1cdefc41e15658f7dcc259565eab701
alexeyvax <alexeyvax@mail.ru>
2017-01-11 00:01:33 +0300

    add rollup-plugin-node-resolve plagin

--- rollup-build.js
+++ rollup-build.js
@@ -1,4 +1,5 @@
 import { rollup } from 'rollup';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-js';
@@ -6,11 +7,20 @@ import { minify } from 'uglify-js';
 rollup({
 		entry: 'scripts/index.js',
 		plugins: [
+			nodeResolve({
+				module: true,
+				jsnext: true,
+				main: true,
+				skip: [ 'some-big-dependency' ],
+				browser: true,
+				extensions: [ '.js', '.json' ],
+				preferBuiltins: false
+			}),
 			babel({
 				exclude: 'node_modules/**',
 				presets: 'es2015-rollup'
 			}),
-			uglify( {}, minify ) // activate when you need to uglify
+			/*uglify( {}, minify )*/ // activate when you need to uglify
 		]
 	}).then( ( bundle ) =>
 	{
