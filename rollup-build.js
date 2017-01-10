114b833c2ca2dd4d5ea69d5232a340a19b9d3b0d
alexeyvax <alexeyvax@mail.ru>
2017-01-10 22:41:30 +0300

    Update to version 0.1.8

--- rollup-build.js
+++ rollup-build.js
@@ -1,5 +1,4 @@
 import { rollup } from 'rollup';
-import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-js';
@@ -7,20 +6,16 @@ import { minify } from 'uglify-js';
 rollup({
 		entry: 'scripts/index.js',
 		plugins: [
-			commonjs({
-				include: 'node_modules/**'
-			}),
 			babel({
-				// exclude: 'node_modules/**',
+				exclude: 'node_modules/**',
 				presets: 'es2015-rollup'
 			}),
 			uglify( {}, minify ) // activate when you need to uglify
-			/*uglify( {}, minify )*/ // activate when you need to uglify
 		]
 	}).then( ( bundle ) =>
 	{
 		return bundle.write({
-			format: 'cjs',
+			format: 'iife',
 			dest: 'public/index.js',
 			moduleName: 'formValidationConstructor'
 		});
