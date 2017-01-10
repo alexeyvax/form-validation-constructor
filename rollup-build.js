27d5bb6f7e37dbb82dd96fa870f8d8358fac76a6
alexeyvax <alexeyvax@mail.ru>
2017-01-10 17:51:58 +0300

    Change webpack on rollup Changed from webpack to rollup.

---
+++ rollup-build.js
@@ -0,0 +1,27 @@
+import { rollup } from 'rollup';
+
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-js';
+
+rollup({
+		entry: 'scripts/index.js',
+		plugins: [
+			babel({
+				exclude: 'node_modules/**',
+				presets: 'es2015-rollup'
+			}),
+			uglify( {}, minify )
+		]
+	}).then( ( bundle ) =>
+	{
+		return bundle.write({
+			format: 'iife',
+			dest: 'public/index.min.js',
+			moduleName: 'formValidationConstructor'
+		});
+	}).then( () =>
+	{
+		console.log( 'Сборка окончена' );
+	}
+);
