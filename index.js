8ed81d31136587a76edd50ea1f39d00d4f640c4a
alexeyvax <Alexeyvax@mail.ru>
2017-11-05 19:03:44 +0300

    update to 0.6.3 version

---
+++ index.js
@@ -0,0 +1,5 @@
+var formValidationConstructor = require('./public/index.min.js');
+var tooltipStyles = require('./public/style.css');
+
+module.exports = formValidationConstructor;
+module.exports = tooltipStyles;
